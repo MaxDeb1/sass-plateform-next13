99ffa83da14f6cb9f022bf2ebb5528f3904ddaa0
MaxDeb1 <deb1maxime@gmail.com>
2023-10-11 14:49:39 +0200

    Product Page & File Uploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
+import { Toaster } from "@/components/ui/toaster"
 
 import 'react-loading-skeleton/dist/skeleton.css'
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         )}>
           <Navbar />
           {children}
+          <Toaster />
         </body>
       </Providers>
     </html>
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,8 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { privateProcedure, publicProcedure, router } from "./trpc";
 import { z } from "zod";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -39,28 +39,44 @@ export const appRouter = router({
       },
     });
   }),
-  deleteFile: privateProcedure.input(
-    z.object({ id: z.string() })
-  ).mutation(async ({ctx, input}) => {
-    const { userId } = ctx
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
 
-    const file = await db.file.findFirst({
-      where: {
-        id: input.id,
-        userId
-      },
-    })
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
 
-    if(!file) throw new TRPCError({ code: 'NOT_FOUND' })
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" }); 
 
-    await db.file.delete({
-      where: {
-        id: input.id
-      },
-    })
+      return file;
+    }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
 
-    return file
-  })
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
